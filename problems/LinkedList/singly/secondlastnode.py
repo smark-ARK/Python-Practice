2d41aebfbd7c69e8270302d320e12c6fc4e10d90
smark-ARK <arkhangreat2@gmail.com>
2022-02-17 11:33:39 +0530

    solving second last node

---
+++ problems/LinkedList/singly/secondlastnode.py
@@ -0,0 +1,46 @@
+from queue import Empty
+
+
+class SinglyLinkedList:
+    class _Node:
+        __slots__ = ["element", "next"]
+
+        def __init__(self, element, next):
+            self.element = element
+            self.next = next
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Empty("List is Empty")
+        return self.head.element
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty("Queue is Empty")
+        answer = self.head.element
+        self.head = self.head.next
+        self.size -= 1
+        if self.is_empty():
+            self.tail = None
+        return answer
+
+    def enqueue(self, e):
+        newest = self._Node(e, None)
+        if self.is_empty():
+            self.head = newest
+        else:
+            self.tail.next = newest
+        self.tail = newest
+        self.size += 1
+
